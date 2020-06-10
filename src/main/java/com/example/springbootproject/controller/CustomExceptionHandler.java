6c7425c7ca773e5a335f54186b5cc78083569761
Romina Saucedo <rsaucedo@integralads.com>
2020-06-10 09:16:49 -0500

    Add ResponseEntity

--- src/main/java/com/example/springbootproject/controller/CustomExceptionHandler.java
+++ src/main/java/com/example/springbootproject/controller/CustomExceptionHandler.java
@@ -1,18 +1,17 @@
 package com.example.springbootproject.controller;
 
 import com.example.springbootproject.exception.ServiceException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+
     @ExceptionHandler(ServiceException.class)
-    public String handleException(ServiceException ex)
+    public ResponseEntity handleException(ServiceException ex)
     {
-        return ex.getMessage();
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
 }
